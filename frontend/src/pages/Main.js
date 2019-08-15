f90a505d1cb366d0168b12aad5a97d8d2a2e784c
Henrique Omena <88lmao@gmail.com>
2019-08-16 00:02:09 +0100

    Routes Working

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>component main.js</h1>
+            </div>
+        )
+    }
+}
