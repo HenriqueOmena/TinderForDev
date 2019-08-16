d1c730f299fa56d8a060ed8b968f1e62864eaf5a
Henrique Omena <88lmao@gmail.com>
2019-08-17 00:10:06 +0100

    done layout  css show dev

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
+import './Main.css'
 
 import logo from '../assets/logo.svg'
+import dislike from '../assets/dislike.svg'
+import like from '../assets/like.svg'
 
 export default function Main({ match }) {
 
@@ -9,15 +12,80 @@ export default function Main({ match }) {
             <img src={logo} alt=""/>
             <h1>{ match.params.id }</h1>
             <ul>
+
                 <li>
-                    <img src={logo} alt=""/>
+                    <img src="https://avatars2.githubusercontent.com/u/35804326?v=4" alt=""/>
                     <footer>
                         <strong>
                             Henrique Omena
                         </strong>
                         <p> Descrição aqui </p>
                     </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt=""/>
+                        </button>
+                        <button type="button">
+                            <img src={like} alt=""/>
+                        </button>
+                    </div>
                 </li>
+
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/35804326?v=4" alt=""/>
+                    <footer>
+                        <strong>
+                            Henrique Omena
+                        </strong>
+                        <p> Descrição aqui </p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt=""/>
+                        </button>
+                        <button type="button">
+                            <img src={like} alt=""/>
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/35804326?v=4" alt=""/>
+                    <footer>
+                        <strong>
+                            Henrique Omena
+                        </strong>
+                        <p> Descrição aqui </p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt=""/>
+                        </button>
+                        <button type="button">
+                            <img src={like} alt=""/>
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/35804326?v=4" alt=""/>
+                    <footer>
+                        <strong>
+                            Henrique Omena
+                        </strong>
+                        <p> Descrição aqui </p>
+                    </footer>
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt=""/>
+                        </button>
+                        <button type="button">
+                            <img src={like} alt=""/>
+                        </button>
+                    </div>
+                </li>
+
+
             </ul>
         </div>
     )
