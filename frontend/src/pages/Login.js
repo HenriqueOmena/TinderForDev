4fb342e349ab3301833c3adfdf806dfd5c8bf878
Henrique Omena <88lmao@gmail.com>
2019-08-16 23:50:44 +0100

    route created and comunicatyion with component

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css'
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg'
 
-function Login() {
+function Login({ history }) {
+    const [ username, setUsername ] = useState('');
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        const response = await api.post('/devs', {
+            username,
+        });
+
+        const { _id } = response.data;
+
+        history.push(`/dev/${_id}`);
+
+        console.log('aqui o response', response)
+        console.log(username);
+    }
+
     return (
         <div className="login-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <img src={logo} alt="tinDev"></img>
-                <input type="text" placeholder="Digite Utilizador  Github"/>
+                <input
+                    type="text"
+                    placeholder="Digite Utilizador  Github"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                    />
                 <button type="submit">Enviar</button>
             </form>
         </div>
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 
-export default class Main extends Component {
-    render() {
-        return (
-            <div>
-                <h1>component main.js</h1>
-            </div>
-        )
-    }
+import logo from '../assets/logo.svg'
+
+export default function Main({ match }) {
+
+    return (
+        <div className="main-container">
+            <img src={logo} alt=""/>
+            <h1>{ match.params.id }</h1>
+            <ul>
+                <li>
+                    <img src={logo} alt=""/>
+                    <footer>
+                        <strong>
+                            Henrique Omena
+                        </strong>
+                        <p> Descrição aqui </p>
+                    </footer>
+                </li>
+            </ul>
+        </div>
+    )
+
 }
